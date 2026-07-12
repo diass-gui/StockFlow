f913495a23c3716523a77c3f70c7d23052f4b495
diass-gui <enriqueguilherme2@gmail.com>
2026-07-12 19:35:28 -0300

    Update: Criação dos métodos de listar e salvar na Service da Entidade Usuario.

--- StockFlow-backend/src/main/java/com/guilhermeDias/StockFlow/service/UsuarioService.java
+++ StockFlow-backend/src/main/java/com/guilhermeDias/StockFlow/service/UsuarioService.java
@@ -1,4 +1,20 @@
 package com.guilhermeDias.StockFlow.service;
 
+import com.guilhermeDias.StockFlow.entity.Usuario;
+import com.guilhermeDias.StockFlow.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    public List<Usuario> listarTodos() { return repository.findAll(); }
+
+    public void salvar(Usuario usuario) { repository.save(usuario); }
+
 }
