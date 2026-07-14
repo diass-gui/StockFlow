223f2450727eb291ed510009b529dbe3ef46203d
diass-gui <enriqueguilherme2@gmail.com>
2026-07-13 21:14:42 -0300

    Update: Criação da classe ValidationErrorResponse para aprimoramento do tratamento de erros oriundos da exceção MethodArgumentNotValidException.

--- StockFlow-backend/src/main/java/com/guilhermeDias/StockFlow/exception/ErrorResponse.java
+++ StockFlow-backend/src/main/java/com/guilhermeDias/StockFlow/exception/ErrorResponse.java
@@ -2,9 +2,6 @@ package com.guilhermeDias.StockFlow.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
-import org.springframework.http.HttpStatus;
-
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
--- StockFlow-backend/src/main/java/com/guilhermeDias/StockFlow/exception/ValidationErrorResponse.java
+++ StockFlow-backend/src/main/java/com/guilhermeDias/StockFlow/exception/ValidationErrorResponse.java
@@ -1,16 +1,10 @@
 package com.guilhermeDias.StockFlow.exception;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.springframework.http.HttpStatus;
-
 import java.time.LocalDateTime;
 import java.util.Map;
 
 @Getter
-@Setter
 public class ValidationErrorResponse extends ErrorResponse {
     public ValidationErrorResponse(LocalDateTime timestamp, Integer status, String error, String message, String path, Map<String, String> erros) {
         super(timestamp, status, error, message, path);
